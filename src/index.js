5a1287697d8bd2689be90b00fe60467051562896
chelovek-zeml <stranger37561@gmail.com>
2022-08-08 16:11:39 +0300

    add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+
+  let flag = false;
+  let temp_str = '';
+  let arr = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    arr.push(bracketsConfig[i]);
+  }
+
+  if (str.length % 2 === 0) {
+      let count = Math.round(str.length / 2);
+      while (count > 0) {
+        for (let i = 0; i < arr.length; i++) {
+          temp_str = arr[i][0] + arr[i][1];
+          str = str.replace(temp_str, '');
+          if (str.length === 0) {
+            flag = true;
+          }
+        }
+        count--;
+      }
+  }
+
+  console.log(flag);
+  return flag;
+};
